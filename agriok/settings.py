c4a83a7d6176d0f72a98d455f994d1b6b887ee74
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2024-11-03 10:18:44 +0100

    Setting append_slash to true so that the url can be accessed without a trailing /

--- agriok/settings.py
+++ agriok/settings.py
@@ -28,6 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-et01d@f83)sb&v+waeh(p@i3uvwqc+l)iwp**8o%z!mc6j^09g"
 
+APPEND_SLASH = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

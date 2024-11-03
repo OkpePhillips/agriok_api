e9f5c0359c13a0f8351e191db2e0e0870966d0b9
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2024-11-03 09:55:15 +0100

    Working on cors-origin allow

--- agriok/settings.py
+++ agriok/settings.py
@@ -177,4 +177,7 @@ if not DEBUG:
 
 SWAGGER_SETTINGS = {"USE_SESSION_AUTH": False}
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://agri-ok.vercel.app/",
+    "http://localhost:5173",
+]

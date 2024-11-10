b1c9edfe0fd588a9439ce50844f7d374286ac510
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2024-11-10 17:50:36 +0100

    Fixed missing formParser

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from django.conf import settings
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -537,6 +537,8 @@ class ProductDetailAPIView(APIView):
     """
     View to retrieve, update and delete specific products.
     """
+
+    parser_classes = (FormParser, MultiPartParser)
 
     def get_permissions(self):
         if self.request.method == "GET":

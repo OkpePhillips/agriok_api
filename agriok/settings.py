4491cf0e837bceb541c5d6067aba991fc56bb91e
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2024-11-10 18:54:24 +0100

    Removed duplicate application label

--- agriok/settings.py
+++ agriok/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api",
     "rest_framework",
     "rest_framework_swagger",
     "drf_yasg",
